53515baed8ebdd45186580fba7e31f74f2b9aa12
estervinia <viniaester@apps.ipb.ac.id>
2026-09-15 18:19:56 +0700

    Menambahkan fungsi perhitungan diskon

--- panen.py
+++ panen.py
@@ -4,13 +4,17 @@ def hitung_total_hasil(panen):
     return sum(panen)
 
 
+def hitung_diskon(total, persentase_diskon):
+    diskon = total * persentase_diskon / 100
+    return total - diskon
+
+
 # Data hasil panen dalam kilogram
 data_panen = [100, 150, 200, 175]
 
 total = hitung_total_hasil(data_panen)
+total_setelah_diskon = hitung_diskon(total, 10)
 
 print("Data hasil panen:", data_panen)
 print("Total hasil panen:", total, "kg")
-def hitung_diskon(total, persentase_diskon):
-    diskon = total * persentase_diskon / 100
-    return total - diskon
+print("Total setelah diskon 10%:", total_setelah_diskon, "kg")
